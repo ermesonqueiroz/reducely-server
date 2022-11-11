854fa24fa3aeefcfe5f367bec3bca6d42efae86c
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-11 19:00:17 -0300

    feat: create support class to handle errors

---
+++ src/common/either.ts
@@ -0,0 +1,41 @@
+export type Either<L, R> = Left<L, R> | Right<L, R>;
+
+export class Left<L, R> {
+  public readonly value: L;
+
+  constructor(value: L) {
+    this.value = value;
+  }
+
+  public isLeft(): this is Left<L, R> {
+    return true;
+  }
+
+  public isRight(): this is Right<L, R> {
+    return false;
+  }
+}
+
+export class Right<L, R> {
+  public readonly value: R;
+
+  constructor(value: R) {
+    this.value = value;
+  }
+
+  public isLeft(): this is Left<L, R> {
+    return false;
+  }
+
+  public isRight(): this is Right<L, R> {
+    return true;
+  }
+}
+
+export const left = <L, R>(l: L): Either<L, R> => {
+  return new Left<L, R>(l);
+}
+
+export const right = <L, R>(r: R): Either<L, R> => {
+  return new Right<L, R>(r);
+}
