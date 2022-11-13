8bcb87e4694c05a325347622ce08f8c6c3c0b056
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-12 21:31:35 -0300

    refactor: add params to http request

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -5,7 +5,8 @@ import { HttpRequest } from '../../adapters/presentation/controllers/ports/http'
 export function adaptRoute(controller: Controller) {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
-      body: req.body
+      body: req.body,
+      params: req.params,
     };
     const httpResponse = await controller.handle(httpRequest);
     res.status(httpResponse.statusCode).json(httpResponse.body);
