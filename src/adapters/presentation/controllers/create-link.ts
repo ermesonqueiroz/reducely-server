1d0de4cef626cd9cee2d2a86d88e56890b6662e4
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-14 20:34:16 -0300

    fix: use httpResponseUtils instead of httpHelpers

--- src/adapters/presentation/controllers/create-link.ts
+++ src/adapters/presentation/controllers/create-link.ts
@@ -1,25 +1,24 @@
 import { CreateLink } from '../../../usecases/create-link';
 import { MissingParamError } from './errors';
-import { badRequest, ok, serverError } from './helpers/http-helper';
 import { Controller } from './ports/controller';
-import { HttpRequest, HttpResponse } from './ports/http';
+import { HttpRequest, HttpResponseUtils } from './ports/http';
 
 export class CreateLinkController implements Controller {
   constructor(private readonly createLink: CreateLink) {}
 
-  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  public async handle(httpRequest: HttpRequest, httpResponseUtils: HttpResponseUtils): Promise<void> {
     try {
       if (!httpRequest.body.target)
-        return badRequest(new MissingParamError('target'));
+        return httpResponseUtils.badRequest(new MissingParamError('target'));
       
       const linkData = { target: httpRequest.body.target };
       const createLinkResponse = await this.createLink.execute(linkData);
       if (createLinkResponse.isLeft())
-        return badRequest(createLinkResponse.value); 
+        return httpResponseUtils.badRequest(createLinkResponse.value); 
 
-      return ok(createLinkResponse.value);
+      return httpResponseUtils.ok(createLinkResponse.value);
     } catch (error) {
-      return serverError('internal');
+      return httpResponseUtils.serverError('internal');
     }
   }
 }
