1c67696bf7f0ccee43a334155491c1639cdce756
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-16 21:34:58 -0300

    feat: add update method in memory links repository
    This commit implement the update method of Links Repository interface on
    In Memory Links Repository.

--- src/repositories/in-memory/in-memory-links-repository.spec.ts
+++ src/repositories/in-memory/in-memory-links-repository.spec.ts
@@ -70,4 +70,42 @@ describe('In memory Links repository', () => {
     const link = await stu.exists('Z5XEhf66');
     expect(link).toBeFalsy();
   });
+
+  it('should update link if is found', async () => {
+    const createdAt = new Date();
+    const links: LinkData[] = [{
+      id: 'Z5XEhf66',
+      target: 'https://github.com/',
+      accessCount: 0,
+      createdAt,
+    }];
+    const stu = new InMemoryLinkRepository(links);
+    await stu.update('Z5XEhf66', {
+      id: 'Z5XEhf66',
+      target: 'https://github.com/ermesonqueiroz',
+      accessCount: 0,
+      createdAt,
+    });
+    const link = await stu.findLinkById('Z5XEhf66');
+    expect(link?.target).toEqual('https://github.com/ermesonqueiroz');
+  });
+
+  it('should not update link if is not found', async () => {
+    const createdAt = new Date();
+    const links: LinkData[] = [{
+      id: 'Z5XEhf66',
+      target: 'https://github.com/',
+      accessCount: 0,
+      createdAt,
+    }];
+    const stu = new InMemoryLinkRepository(links);
+    await stu.update('XXXXXXXX', {
+      id: 'Z5XEhf66',
+      target: 'https://github.com/ermesonqueiroz',
+      accessCount: 0,
+      createdAt,
+    });
+    const link = await stu.findLinkById('Z5XEhf66');
+    expect(link?.target).toEqual('https://github.com/');
+  });
 });
--- src/repositories/in-memory/in-memory-links-repository.ts
+++ src/repositories/in-memory/in-memory-links-repository.ts
@@ -4,6 +4,14 @@ import { LinksRepository } from '../ports/links-repository';
 export class InMemoryLinkRepository implements LinksRepository {
   constructor(private readonly links: LinkData[]) {}
 
+  public async update(id: string, data: LinkData): Promise<void> {
+    const link = await this.exists(id);
+    if (!link) return;
+
+    const linkIndex = this.links.findIndex(({ id: linkId }) => id === linkId);
+    this.links[linkIndex] = data;
+  }
+
   public async findLinkById(id: string): Promise<LinkData | null> {
     const link = this.links.find((link) => link.id === id);
     if (!link) return null;
