2e155389e630e323951dbba823faea81b76a4ac6
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-16 22:35:03 -0300

    feat: call IncreaseLinkAccessCount usecase
    Call increase link access count use case into redirect controller.

--- src/adapters/presentation/controllers/redirect.ts
+++ src/adapters/presentation/controllers/redirect.ts
@@ -1,10 +1,14 @@
 import { FindLink } from '../../../usecases/find-link';
+import { IncreaseLinkAccessCount } from '../../../usecases/increase-link-acess-count';
 import { MissingParamError } from './errors';
 import { ControllerResponse } from './ports/controller';
 import { HttpRequest, HttpResponseUtils } from './ports/http';
 
 export class RedirectController {
-  constructor(private readonly findLink: FindLink) {}
+  constructor(
+    private readonly findLink: FindLink,
+    private readonly increaseLinkAccessCount: IncreaseLinkAccessCount
+  ) {}
 
   public async handle(httpRequest: HttpRequest, httpResponseUtils: HttpResponseUtils): Promise<ControllerResponse> {
     try {
@@ -13,6 +17,9 @@ export class RedirectController {
 
       const findLinkResponse = await this.findLink.execute({ id: httpRequest.params.id });
       if (findLinkResponse.isLeft()) return httpResponseUtils.badRequest(findLinkResponse.value);
+
+      const increaseLinkAccessCountResponse = await this.increaseLinkAccessCount.execute({ linkId: httpRequest.params.id});
+      if (increaseLinkAccessCountResponse.isLeft()) return httpResponseUtils.badRequest(increaseLinkAccessCountResponse.value);
 
       return httpResponseUtils.redirect(findLinkResponse.value.target);
     } catch (error) {
--- src/main/factories/redirect-factory.ts
+++ src/main/factories/redirect-factory.ts
@@ -1,10 +1,12 @@
 import { RedirectController } from '../../adapters/presentation/controllers/redirect';
 import { PostgresLinksRepository } from '../../external/repositories/postgres/postgres-links-repository';
 import { FindLink } from '../../usecases/find-link';
+import { IncreaseLinkAccessCount } from '../../usecases/increase-link-acess-count';
 
 export function makeRedirectController() {
   const postgresLinksRepository = new PostgresLinksRepository();
   const findLink = new FindLink(postgresLinksRepository);
-  const redirectController = new RedirectController(findLink);
+  const increaseLinkAccessCount = new IncreaseLinkAccessCount(postgresLinksRepository);
+  const redirectController = new RedirectController(findLink, increaseLinkAccessCount);
   return redirectController;
 }
