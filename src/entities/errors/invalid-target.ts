a37f0f669ab6e95945c339087e0f19576ea1a0c3
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2022-11-26 19:41:36 -0300

    chore: update invalid target message error

--- src/entities/errors/invalid-target.ts
+++ src/entities/errors/invalid-target.ts
@@ -2,7 +2,7 @@ import { DomainError } from './domain-error';
 
 export class InvalidTargetError extends Error implements DomainError {
   constructor(name: string) {
-    super(`The target: "${name} is invalid."`);
+    super(`The target "${name}" is invalid.`);
     this.name = 'InvalidTargetError';
   }
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,7 @@
 import { PostgresHelper } from '../external/repositories/postgres/helpers/postgres-helper';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 PostgresHelper.connect(process.env.DATABASE_URL as string)
   .then(async () => {
